a30ee88621373c9cddf2969ddb08a8a8f4058ec4
majere <mrpsihho@gmail.com>
2021-03-08 21:43:37 +0300

    working webhook

--- pepaka.py
+++ pepaka.py
@@ -1,4 +1,6 @@
-import configparser, ssl
+import configparser
+import ssl
+from aiohttp import web
 
 print('It`s me, Pepaka III')
 
@@ -9,16 +11,27 @@ try:
     print(config['default']['bot_api_token'])
     print(config['default']['ssl_fullchain'])
     print(config['default']['ssl_privkey'])
-
+    print(config['default']['webhook_listen'])
+    print(config['default']['webhook_port'])
 
 except LookupError:
     print('config.ini error')
     exit()
 
 
-def webhook_setup():
-    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ssl_context.load_cert_chain(config['default']['ssl_fullchain'], config['default']['ssl_privkey'])
+class Webhook:
+    async def start(request):
+
+        response = await request.text()
+        print('------------------------------------')
+        print(response)
+        return web.Response()
+
+    app = web.Application()
+    app.add_routes([web.post('/', start), web.post('/{name}', start)])
 
 
-webhook_setup()
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(config['default']['ssl_fullchain'], config['default']['ssl_privkey'])
+webhook = Webhook()
+web.run_app(webhook.app, host=config['default']['webhook_listen'], port=config['default']['webhook_port'], ssl_context=ssl_context)
