9f12c841fce4af9f25a7bee1cdf6e4971107f05d
majere <mrpsihho@gmail.com>
2021-03-08 20:49:18 +0300

    add SSL

--- pepaka.py
+++ pepaka.py
@@ -1,4 +1,4 @@
-import configparser
+import configparser, ssl
 
 print('It`s me, Pepaka III')
 
@@ -7,8 +7,18 @@ config = configparser.ConfigParser()
 try:
     config.read('config.ini')
     print(config['default']['bot_api_token'])
+    print(config['default']['ssl_fullchain'])
+    print(config['default']['ssl_privkey'])
+
 
 except LookupError:
     print('config.ini error')
+    exit()
 
 
+def webhook_setup():
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(config['default']['ssl_fullchain'], config['default']['ssl_privkey'])
+
+
+webhook_setup()
