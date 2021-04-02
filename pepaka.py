e5e1aa4b447a0fad441b8ea2065b90d1ef0cbfa4
majere <mrpsihho@gmail.com>
2021-04-02 23:45:26 +0300

    refactoring

--- pepaka.py
+++ pepaka.py
@@ -1,11 +1,11 @@
 import configparser
+import datetime
 import ssl
 import time
 import json
 import requests
 import random
 import string
-import io
 from aiohttp import web
 from threading import Thread
 from sqlalchemy.orm import sessionmaker
@@ -101,8 +101,10 @@ class Message:
     reply_text = None
     file_id = None
     file_path = None
+    bot_owner = None
 
     def __init__(self, data):
+        self.bot_owner = cfg.owner_id
         print(data)
         self.message_id = data['message_id']
         print('message_id =', self.message_id)
@@ -125,7 +127,8 @@ class Message:
             self.command = self.caption.lower()
             print('caption =', self.caption)
         if data.get('photo'):
-            self.file_id = data['photo'][0]['file_id']
+            count = len(data['photo'])
+            self.file_id = data['photo'][count - 1]['file_id']
             print('file_id =', self.file_id)
         if data.get('sticker'):
             self.sticker = data['sticker']
@@ -160,6 +163,7 @@ class Methods:
         data = {'chat_id': chat_id, 'parse_mode': 'HTML', 'text': text}
         r = requests.post(url, data=data)
         print('send message status:', r)
+        print(r.text)
 
     def sendReply(self, chat_id, message_id, text):
         url = self.url + '/sendMessage'
@@ -188,26 +192,37 @@ class Methods:
         print('sendPhoto =', r.text)
 
 
+class Actions:
+    dict_actions = {}
+    is_command = False
+    action = None
+
+    def __init__(self, m):
+        self.dict_actions = {'!del': db.del_user,
+                             '!add': db.add_user,
+                             '!мем': Meme().save_meme,
+                             '!дата': InfinitySummer}
+        self.action = m.command.split(' ')
+        self.action = self.action[0]
+
+    def check_general_actions(self, m):
+        if self.action in self.dict_actions:
+            self.dict_actions[self.action](m)
+            return True
+        else:
+            return False
+
+    def check_db_actions(self, m):
+        print('DB CHECK!', m.command)
+
+
 class PepakaCore:
     def core(message):
         print('core')
         m = Message(message)
-        is_command = True
-        if m.command:
-            if m.command == '!del':
-                db.del_user(m, cfg)
-                is_command = False
-            if m.command.startswith('!add'):
-                db.add_user(m, cfg)
-                is_command = False
-            if m.command.startswith('!мем') and m.file_id:
-                Meme().save_meme(m)
-                is_command = False
-
-            if m.command.startswith('!') and is_command:
-                if db.get_meme(m):
-                    is_command = False
-                    Meme.send_meme(m)
+        a = Actions(m)
+        if not a.check_general_actions(m):
+            a.check_db_actions(m)
 
 
     def service(message):
@@ -260,6 +275,7 @@ class Files:
         jdata = json.loads(data.text)
         if jdata['ok']:
             print('DOWNLOAD')
+            print(jdata)
             m.file_path = jdata['result']['file_path']
             url_file = self.url + '/' + jdata['result']['file_path']
             file = jdata['result']['file_path']
@@ -269,6 +285,33 @@ class Files:
             f.close()
         else:
             text = 'getFile failed'
+            mtd.sendMessage(m.chat_id, text)
+
+
+class InfinitySummer:
+    def __init__(self, m):
+        print('Summer never end')
+        mtd = Methods()
+        # curr_date = datetime.date(2020, 9, 22) # for tests
+        curr_date = datetime.date.today()
+        curr_month = curr_date.month
+        if curr_month < 6:
+            last_summer_day = datetime.date(curr_date.year - 1, 8, 31)
+        else:
+            last_summer_day = datetime.date(curr_date.year, 8, 31)
+
+        if curr_month == 6 or curr_month == 7 or curr_month == 8:
+            if curr_month == 6:
+                month = ' июня '
+            if curr_month == 7:
+                month = ' июля '
+            if curr_month == 8:
+                month = ' августа '
+            text = 'Сегодня ' + str(curr_date.day) + month + str(curr_date.year) + ' года'
+            mtd.sendMessage(m.chat_id, text)
+        else:
+            true_date = (curr_date - last_summer_day).days + 31
+            text = 'Сегодня ' + str(true_date) + ' августа ' + str(curr_date.year) + ' года'
             mtd.sendMessage(m.chat_id, text)
 
 
@@ -292,72 +335,54 @@ class DB:
         self.session = sessionmaker()
         self.session.configure(bind=self.engine)
 
-    def check_user_role(self, m):
-        s = self.session()
-        q = s.query(Admins).filter(Admins.user_id == m.user_id)
-        if q.count() == 1:
-            for elem in q:
-                return elem.role
-        elif q.count() > 1:
-            print('Слишком много записей с таким user_id')
-            return False
-        else:
-            print('Такого user_id нет в бд')
-            return False
-
-    def check_owner(self, conf):
+    def create_tables(self):
         self.base.metadata.create_all(self.engine)
-        print('START CHECK OWNER')
-        s = self.session()
-        q = s.query(Admins).filter(Admins.user_id == conf.owner_id)
-        if q.count() == 1:
-            print('owner ok')
-        elif q.count() > 1:
-            print('too many owners')
-        else:
-            print('No owner. Create owner')
-            t = Admins(user_id=conf.owner_id, role='owner', token=RandomGenerator.genString(64))
-            s.add(t)
-            s.commit()
 
-    def add_user(self, m, conf):
-        print('START ADD USER TO ADMINS')
-        if conf.owner_id == m.user_id and m.reply_user_id:
-            mtd = Methods()
-            s = self.session()
-            q = s.query(Admins).filter(Admins.user_id == m.reply_user_id)
-            if q.count() == 0:
-                t = Admins(user_id=m.reply_user_id, role='admin', token=RandomGenerator.genString(64))
-                s.add(t)
-                s.commit()
-                mtd.sendChatAction(m.chat_id, 'typing')
-                text = F'<b>{m.reply_user_fullname}</b> принят в Котячий дворец'
-                mtd.sendMessage(m.chat_id, text)
+    def add_user(self, m):
+        print('Start add user')
+        mtd = Methods()
+        if m.user_id == m.bot_owner:
+            if m.reply_user_id:
+                s = self.session()
+                q = s.query(Admins).filter(Admins.user_id == m.reply_user_id)
+                if q.count() == 0:
+                    t = Admins(user_id=m.reply_user_id, role='admin', token=RandomGenerator.genString(64))
+                    s.add(t)
+                    s.commit()
+                    mtd.sendChatAction(m.chat_id, 'typing')
+                    text = F'<b>{m.reply_user_fullname}</b>, я тебя запомнил '
+                    mtd.sendMessage(m.chat_id, text)
+                else:
+                    mtd.sendChatAction(m.chat_id, 'typing')
+                    text = F'<b>{m.user_fullname}</b>, ты повторяешься'
+                    mtd.sendMessage(m.chat_id, text)
             else:
                 mtd.sendChatAction(m.chat_id, 'typing')
-                text = F'<b>{m.reply_user_fullname}</b> уже во дворце'
+                text = F'Глупый <b>{m.user_fullname}</b> пытается всё заполнить пустотой'
                 mtd.sendMessage(m.chat_id, text)
         else:
-            print('no rights to add admin')
+            mtd.sendChatAction(m.chat_id, 'typing')
+            text = f'<b>{m.user_fullname}</b>, тебе не позволено'
+            mtd.sendMessage(m.chat_id, text)
 
-    def del_user(self, m, conf):
-        print('START DELETE USER')
+    def del_user(self, m):
+        print('Start del user')
         mtd = Methods()
-        if m.reply_user_fullname:
-            if conf.owner_id == m.user_id and m.reply_user_id and m.reply_user_id != conf.owner_id:
+        if m.user_id == m.bot_owner:
+            if m.reply_user_id:
                 s = self.session()
                 s.query(Admins).filter(Admins.user_id == m.reply_user_id).delete()
                 s.commit()
-                text = F'<b>{m.user_fullname}</b> с позором выгнал из котячей крепости <b>{m.reply_user_fullname}</b>'
                 mtd.sendChatAction(m.chat_id, 'typing')
+                text = F'<b>{m.user_fullname}</b> зачеркнул <b>{m.reply_user_fullname}</b>'
                 mtd.sendMessage(m.chat_id, text)
             else:
-                text = F'У <b>{m.user_fullname}</b> нет прав для удаления <b>{m.reply_user_fullname}</b>'
                 mtd.sendChatAction(m.chat_id, 'typing')
+                text = F'Глупый <b>{m.user_fullname}</b> пытается удалить пустоту'
                 mtd.sendMessage(m.chat_id, text)
         else:
-            text = 'Пустота вечна. Её нельзя удалить'
             mtd.sendChatAction(m.chat_id, 'typing')
+            text = f'<b>{m.user_fullname}</b>, тебе не позволено'
             mtd.sendMessage(m.chat_id, text)
 
     def add_meme(self, m):
@@ -386,11 +411,9 @@ class DB:
         q = s.query(Memes).filter(Memes.chat_id == m.chat_id, Memes.command == m.command)
         if q.count() == 1:
             m.file_path = q[0].file
-            print(m.file_path)
             return True
         else:
             return False
-
 
 
 db = DB(cfg)
@@ -413,7 +436,13 @@ class Memes(db.base):
     file = Column('file', String)
 
 
-db.check_owner(cfg)
+class Commands(db.base):
+    __tablename__ = 'commands'
+    id = Column(Integer, primary_key=True)
+    command = Column('command', String)
+    func = Column('func', String)
+
+db.create_tables()
 # start web-server
 ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_context.load_cert_chain(cfg.ssl_fullchain, cfg.ssl_privkey)
